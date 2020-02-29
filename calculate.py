6b99e8c29f55b4565a27824fefb036b4c2f48a5d
Josh Coles <josh@joshcoles.com>
2020-02-29 10:56:12 -0500

    Create index.py that imports calculate module with distanceToDuff method

---
+++ calculate.py
@@ -0,0 +1,23 @@
+from math import sin, cos, sqrt, atan2, radians
+
+def distanceToDuff(lat1, lon1):
+     
+  R = 6373.0
+  
+  duffLat = radians(43.655830)
+  duffLon = radians(-79.435360)
+
+  lat2 = duffLat
+  lon2 = duffLon
+
+  dlon = lon2 - lon1
+  dlat = lat2 - lat1
+
+  a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+  c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+  distance = R * c
+
+  x = distance
+
+  print(x)
---
+++ index.py
@@ -0,0 +1,5 @@
+from math import radians
+
+import calculate
+
+calculate.distanceToDuff(radians(43.669378), radians(-79.338253))
